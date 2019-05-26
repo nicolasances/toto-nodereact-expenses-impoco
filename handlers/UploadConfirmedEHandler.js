dfd62032fae787c628e7ba4c7cd5220d644494fc
nicolas <nicolas.matteazzi@gmail.com>
2019-05-26 11:13:56 +0200

    Posting expense to be posted

--- handlers/UploadConfirmedEHandler.js
+++ handlers/UploadConfirmedEHandler.js
@@ -1,9 +1,49 @@
 var totoEventPublisher = require('toto-event-publisher');
+var http = require('toto-request');
+var logger = require('toto-logger');
 
 exports.do = (event) => {
 
-  console.log(event);
+  // Extract the month id and other data
+  let monthId = event.monthId;
+  let correlationId = event.correlationId;
 
-  // totoEventPublisher.publishEvent('trainingSessionsCreated', event);
+  // Retrieve the month data
+  http({
+    correlationId: correlationId,
+    microservice: 'toto-nodems-expenses-import',
+    method: 'GET',
+    resource: '/uploads/' + monthId
+  }).then((data) => {
+
+    if (data == null || data.expenses == null) {
+      logger.compute(correlationId, 'No expenses in month ' + monthId, 'info');
+      return;
+    }
+
+    // Post the expenses
+    for (var i = 0; i < data.expenses.length; i++) {
+
+      let exp = data.expenses[i];
+
+      // Skip positive amounts
+      if (exp.amount > 0) continue;
+
+      // Create the post expense event
+      expEvent = {
+        date: exp.date,
+        amount: Math.abs(exp.amount),
+        currency: exp.currency ? exp.currency : 'EUR',
+        description: exp.description,
+        category: 'VARIE',
+        yearMonth: data.yearMonth,
+        user: data.user
+      }
+
+      // Post each expense on an event
+      totoEventPublisher.publishEvent('expenseToBePosted', expEvent);
+
+    }
+  })
 
 }
